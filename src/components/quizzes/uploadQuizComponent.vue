6dde5075f23d162a40d0739b7fb5c60ec463cb17
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-11-21 08:16:21 +0000

    worked on upload forms

--- src/components/quizzes/uploadQuizComponent.vue
+++ src/components/quizzes/uploadQuizComponent.vue
@@ -48,7 +48,15 @@ watch(fields, (newFields) => {
   <form @submit.prevent class="w-full my-16">
     <h1 class="text-2xl w-full text-center font-semibold mb-6">Enter quiz details below</h1>
     <div class="grid grid-cols-3 gap-x-3 gap-y-6">
-      <custom-input @set-data="handleChange" v-for="field in inputFields" :value="fields[field.toLowerCase()].value"  type="text" :label="field" :is-error="!!fields[field.toLowerCase()].error"></custom-input>
+      <custom-input
+          @set-data="handleChange"
+          v-for="field in inputFields"
+          :value="fields[field.toLowerCase()].value"
+          type="text"
+          :label="field"
+          :is-error="!!fields[field.toLowerCase()].error">
+
+      </custom-input>
       <div @click="showOptions = !showOptions" class="w-full relative flex items-center h-10 px-3 border-2 rounded-lg border-[#9CA3AF] text-[#9CA3AF]">
         <span>Level</span>
         <div v-if="showOptions" class="w-full absolute flex flex-col top-10 left-0 shadow-lg rounded-lg">
